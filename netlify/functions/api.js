79ea49f402ca2de3c3c432df087efebabadede47
Adarsh Raveendran <adarsh@Adarshs-MacBook-Pro.local>
2026-01-22 01:40:15 +0530

    fix(netlify): enable binary response mode for images

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../../src/server');
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+    binary: ['image/png', 'image/jpeg', 'font/ttf']
+});
